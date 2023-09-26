919b86fa9f09e9486ec96ff575930401fdf73afb
andrei <g@g.com>
2023-09-26 09:43:37 +0300

    changed frontend pointing to backend

--- backend/app.ts
+++ backend/app.ts
@@ -23,12 +23,12 @@ import bankTransferRoutes from "./banktransfer-routes";
 import testDataRoutes from "./testdata-routes";
 import { checkAuth0Jwt, verifyOktaToken, checkCognitoJwt, checkGoogleJwt } from "./helpers";
 import resolvers from "./graphql/resolvers";
-import { frontendPort, getBackendPort } from "../src/utils/portUtils";
+import { getBackendPort } from "../src/utils/portUtils";
 
 require("dotenv").config();
 
 const corsOption = {
-  origin: `http://localhost:${frontendPort}`,
+  origin: `https://gorgeous-strudel-9ed118.netlify.app/`,
   credentials: true,
 };
 
